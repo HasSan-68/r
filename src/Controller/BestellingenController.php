8de2b220fd29b835a3f05a778b5106e5e0e69c78
HasSan-68 <48790045+HasSan-68@users.noreply.github.com>
2021-01-19 21:49:07 +0100

    .

--- src/Controller/BestellingenController.php
+++ src/Controller/BestellingenController.php
@@ -10,6 +10,9 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+
+use Dompdf\Dompdf;
+use Dompdf\Options;
 /**
  * @Route("/bestellingen")
  */
@@ -33,7 +36,37 @@ class BestellingenController extends AbstractController
         ]);
         }
     }
+    /**
+     * @Route("/pdf/{id}", name="bestellingen_pdf", methods={"GET"})
+     */
+    public function streamPDF(Bestellingen $bestellingen): Response{
+        $pdfOptions = new Options();
+        $pdfOptions->set('defaultFont', 'Arial');
 
+        // Instantiate Dompdf with our options
+        $dompdf = new Dompdf($pdfOptions);
+
+        // Retrieve the HTML generated in our twig file
+        $html = $this->renderView('bestellingen/show.html.twig', [
+            'bestellingen' => $bestellingen,
+        ]);
+        // Load HTML to Dompdf
+        $dompdf->loadHtml($html);
+
+        // (Optional) Setup the paper size and orientation 'portrait' or 'portrait'
+        $dompdf->setPaper('A4', 'portrait');
+
+        // Render the HTML as PDF
+        $dompdf->render();
+
+        ob_end_clean();
+        // Output the generated PDF to Browser (force download)
+        $dompdf->stream("bestellingen.pdf", [
+            "Attachment" => true
+        ]);
+
+        exit;
+    }
 
 
     /**
